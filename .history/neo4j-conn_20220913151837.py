a54e699c6d35d836304358ffdde2ba5a11842511
themaxermister <subscribingalerts@gmail.com>
2022-09-15 09:32:54 +0800

    INIT

--- .history/neo4j-conn_20220913151837.py
+++
@@ -1,8 +0,0 @@
-from neo4j import GraphDatabase
-
-driver = GraphDatabase.driver("neo4j://localhost:7687",
-                              auth=("neo4j", "password"))
-
-with driver.session(database="NBA DBMS"
-
-driver.close()
--- .history/neo4j-conn_20220913151900.py
+++
@@ -1,8 +0,0 @@
-from neo4j import GraphDatabase
-
-driver = GraphDatabase.driver("neo4j://localhost:7687",
-                              auth=("neo4j", "password"))
-
-session = driver.session(database="neo4j")
-
-driver.close()
